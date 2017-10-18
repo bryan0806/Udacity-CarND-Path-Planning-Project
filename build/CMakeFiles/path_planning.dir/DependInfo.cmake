ee851f46c7be2a653926d86074432a47e24a4518
Chunwei Liu(bryan liu) <chunwei.liu@gmail.com>
2017-10-18 10:40:32 +0800

    Adding pics for repor

--- build/CMakeFiles/path_planning.dir/DependInfo.cmake
+++ build/CMakeFiles/path_planning.dir/DependInfo.cmake
@@ -4,14 +4,12 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Volumes/SecondDataDisk/Udacity self-driving car nanodegree/Udacity-CarND-Path-Planning-Project/src/main.cpp" "/Volumes/SecondDataDisk/Udacity self-driving car nanodegree/Udacity-CarND-Path-Planning-Project/build/CMakeFiles/path_planning.dir/src/main.cpp.o"
+  "/home/ubuntu16/Udacity-SDCND/Udacity-CarND-Path-Planning-Project/src/main.cpp" "/home/ubuntu16/Udacity-SDCND/Udacity-CarND-Path-Planning-Project/build/CMakeFiles/path_planning.dir/src/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include"
-  "/usr/local/opt/openssl/include"
   )
 
 # Targets to which this target links.
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Volumes/SecondDataDisk/Udacity self-driving car nanodegree/Udacity-CarND-Path-Planning-Project
+# Install script for directory: /home/ubuntu16/Udacity-SDCND/Udacity-CarND-Path-Planning-Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -35,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Volumes/SecondDataDisk/Udacity self-driving car nanodegree/Udacity-CarND-Path-Planning-Project/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ubuntu16/Udacity-SDCND/Udacity-CarND-Path-Planning-Project/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
